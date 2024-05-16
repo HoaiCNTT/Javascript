0cb67aea0da56bd9504917f35faa13d331b393da
HoaiCNTT <145319549+HoaiCNTT@users.noreply.github.com>
2024-05-16 17:35:41 +0700

    Delete VoBaHoai_2122110571_Buoi01 directory

--- VoBaHoai_2122110571_Buoi01/bai1.js
+++
@@ -1,6 +0,0 @@
-let masv1=2122110571
-let hoten1="Võ Bá Hoài"
-console.log(masv1);
-console.log(hoten1);
-
-document.getElementById("show").innerHTML=`Mã sinh viên: "${masv1}"Họ tên sinh viên: ${hoten1}`;
